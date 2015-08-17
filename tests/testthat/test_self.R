7c90a3126ac7bc10f19623119d287daebdc1a42f
terrytangyuan <terrytangyuan@gmail.com>
2015-08-17 13:38:26 -0400

    more tests for SELF

--- tests/testthat/test_self.R
+++ tests/testthat/test_self.R
@@ -13,9 +13,9 @@ test_that('self works', {
   # case when r=d
   expect_that(self(X,Y,beta = 0.1, r = 4, metric = "plain"), not(throws_error()))
 
-#   model <- lfda(k,y,r=4,metric="plain")
-#   expect_that(predict(model, iris[,-5]), not(throws_error()))
-#   expect_error(predict(model, iris[,-5], "wrongType"))
-#   expect_error(predict(model))
-#   expect_that(capture.output(print(model)), not(throws_error())) # suppress printing
+  model <- self(X,Y,beta = 0.1, r = 3, metric = "plain")
+  expect_that(predict(model, iris[,-5]), not(throws_error()))
+  expect_error(predict(model, iris[,-5], "wrongType"))
+  expect_error(predict(model))
+  expect_that(capture.output(print(model)), not(throws_error())) # suppress printing
 })
